342f77b97beb69d2aa4bf34d5d68fa71bef39a73
David vasquez <vasquezdavid325@gmail.com>
2024-10-27 22:41:41 -0300

    Se agrego repositorio autenticacion e historial

---
+++ e-Commerce.Muebles/Repos/AutenticacionRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using e_Commerce.Muebles.Entidades;
+
+namespace e_Commerce.Muebles.Repos
+{
+    public interface IAuthRepositorio
+    {
+        Autenticacion GetAutenticacionByEmail(string email);
+        void AddAutenticacion(Autenticacion autenticacion);
+        void UpdateAutenticacion(Autenticacion autenticacion);
+        void DeleteAutenticacion(int id);
+    }
+
+    public class AuthRepos : IAuthRepositorio
+    {
+        private readonly SqlConnection _connection;
+
+        public AuthRepos(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public Autenticacion GetAutenticacionByEmail(string email)
+        {
+            return _connection.QuerySingleOrDefault<Autenticacion>("SELECT * FROM Autenticacion WHERE Email = @Email", new { Email = email });
+        }
+
+        public void AddAutenticacion(Autenticacion autenticacion)
+        {
+            _connection.Execute("INSERT INTO Autenticacion (Email, Clave, UsuarioId) VALUES (@Email, @Clave, @UsuarioId)", autenticacion);
+        }
+
+        public void UpdateAutenticacion(Autenticacion autenticacion)
+        {
+            _connection.Execute("UPDATE Autenticacion SET Email = @Email, Clave = @Clave WHERE IdAtenticacion = @Id", autenticacion);
+        }
+
+        public void DeleteAutenticacion(int id)
+        {
+            _connection.Execute("DELETE FROM Autenticacion WHERE IdAtenticacion = @Id", new { Id = id });
+        }
+    }
+}
---
+++ e-Commerce.Muebles/Repos/HistorialRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using e_Commerce.Muebles.Entidades;
+
+namespace e_Commerce.Muebles.Repos
+{
+    public interface IHistorialRepositorio
+    {
+        IEnumerable<Historial> GetAllHistorial();
+        IEnumerable<Historial> GetHistorialByUsuarioId(int usuarioId);
+        void AddHistorial(Historial historial);
+        void DeleteHistorial(int id);
+    }
+
+    public class HistorialRepos : IHistorialRepositorio
+    {
+        private readonly SqlConnection _connection;
+
+        public HistorialRepos(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<Historial> GetAllHistorial()
+        {
+            return _connection.Query<Historial>("SELECT * FROM Historial");
+        }
+
+        public IEnumerable<Historial> GetHistorialByUsuarioId(int usuarioId)
+        {
+            return _connection.Query<Historial>("SELECT * FROM Historial WHERE UsuarioId = @UsuarioId", new { UsuarioId = usuarioId });
+        }
+
+        public void AddHistorial(Historial historial)
+        {
+            _connection.Execute("INSERT INTO Historial (UsuarioId, VentaId) VALUES (@UsuarioId, @VentaId)", historial);
+        }
+
+        public void DeleteHistorial(int id)
+        {
+            _connection.Execute("DELETE FROM Historial WHERE IdHistorial = @Id", new { Id = id });
+        }
+    }
+}
