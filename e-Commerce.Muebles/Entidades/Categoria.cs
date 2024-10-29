633ba08cb4d5591a00035989f813f2b5c7143e6c
Karen <karenymorel@gmail.com>
2024-10-29 18:44:56 -0300

    Entidad faltante

---
+++ e-Commerce.Muebles/Entidades/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Commerce.Muebles.Entidades
+{
+    public class Categoria
+    {
+        public int id_categoria {  get; set; }
+        public string categoria {  get; set; }
+    }
+}
