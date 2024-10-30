009194bdd4927067fa82e7a4e919f5f122adc6b5
Karen <karenymorel@gmail.com>
2024-10-30 16:14:47 -0300

    Cambio atributos

--- e-Commerce.Muebles.WebAdmin/Models/ProductoModel.cs
+++ e-Commerce.Muebles.WebAdmin/Models/ProductoModel.cs
@@ -4,25 +4,25 @@ namespace e_Commerce.Muebles.WebAdmin.Models
 {
     public class ProductoModel
     {
-        public int IdProducto { get; set; }
+        public int id_producto { get; set; }
 
         [Required(ErrorMessage = "El nombre del producto es obligatorio")]
         [StringLength(100, ErrorMessage = "El nombre no puede exceder de 100 caracteres")]
-        public string Nombre { get; set; }
+        public string nombre { get; set; }
 
         [StringLength(250, ErrorMessage = "La descripción no puede exceder de 250 caracteres")]
-        public string Descripcion { get; set; }
+        public string descripcion { get; set; }
 
         [Range(0.01, 10000.00, ErrorMessage = "El precio debe estar entre 0.01 y 10000.00")]
-        public double Precio { get; set; }
+        public double precio { get; set; }
 
         [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
-        public int Stock { get; set; }
+        public int stock { get; set; }
 
         [Required(ErrorMessage = "El proveedor es obligatorio")]
-        public int ProveedorId { get; set; }
+        public int proveedor_id { get; set; }
 
         [Required(ErrorMessage = "La categoría es obligatoria")]
-        public int CategoriaId { get; set; }
+        public int categoria_id { get; set; }
     }
 }
