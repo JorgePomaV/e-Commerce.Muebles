0e4fc0f40e97a07ba7a066962a47080477cdacea
David vasquez <vasquezdavid325@gmail.com>
2024-11-01 10:33:09 -0300

    Se agrego Producto Model Factory

---
+++ e-Commerce.Muebles/ModelFactories/ProductoModelFactory.cs
@@ -0,0 +1,22 @@
+﻿using e_Commerce.Muebles.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Commerce.Muebles.ModelFactories
+{
+    public class ProductoCompleto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+
+        // Relación con categoría
+        public Categoria Categoria { get; set; }
+
+    }
+}
