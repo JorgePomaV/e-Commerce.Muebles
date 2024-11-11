502ea4796114b4390bdb518b065d02d11d7caf47
JorgePomaV <jpomavergara1418@gmail.com>
2024-11-11 06:03:54 -0300

    Se agrego Boton Cerrar Sesion y Nombre del Usuario logeado

--- e-Commerce.Muebles.WebCliente/Controllers/HomeController.cs
+++ e-Commerce.Muebles.WebCliente/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 
 namespace e_Commerce.Muebles.WebCliente.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
